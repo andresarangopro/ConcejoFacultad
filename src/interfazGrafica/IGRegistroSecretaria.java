bac35c6607aa2d30eb70e47dec7fee402296eda8
Felipe Arango <andresarangopro@gmail.com>
2017-10-22 17:25:11 -0500

    Concejo Facultad

---
+++ src/interfazGrafica/IGRegistroSecretaria.java
@@ -0,0 +1,18 @@
+package interfazGrafica;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class IGRegistroSecretaria extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public IGRegistroSecretaria() {
+		
+		JButton btnNewButton = new JButton("New button");
+		add(btnNewButton);
+
+	}
+
+}
---
+++ src/interfazGrafica/IGReportes.java
@@ -0,0 +1,18 @@
+package interfazGrafica;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class IGReportes extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public IGReportes() {
+		
+		JLabel lblBienvenido = new JLabel("BIENVENIDO!!");
+		add(lblBienvenido);
+
+	}
+
+}
